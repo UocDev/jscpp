605c69d12b81fc6853137e2f783fa499f7ca6dc6
UocDev <vanssenlingga20@gmail.com>
2025-08-21 07:26:54 +0000

    Init errorhandler.hpp

---
+++ runtime/errorhandler.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+
+namespace JSCPP {
+    enum ErrorType {
+        SYNTAX_ERROR,
+        RUNTIME_ERROR,
+        SYSTEM_ERROR,
+        WARNING
+    };
+
+    // Report an error, optionally with line number
+    void throwError(const std::string &message, int line = -1, ErrorType type = SYNTAX_ERROR);
+
+    // Report a warning (non-fatal)
+    void warn(const std::string &message, int line = -1);
+}
